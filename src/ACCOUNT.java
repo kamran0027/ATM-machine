05c2eee5695cfff9bba0670f07c8053cb2de751e
Kamran Ahmad <kamranahmadmd463@gmail.com>
2024-05-04 02:36:59 +0530

    create Account class

---
+++ src/ACCOUNT.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ACCOUNT {
+    float bal;
+     int pin;
+    Scanner sc=new Scanner(System.in);
+
+    ACCOUNT(float bal,int pin){
+       this.bal=bal;
+       this.pin=pin;
+   }
+
+
+
+}
