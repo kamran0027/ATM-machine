f161b1e9216d998adefe69ddab11eecd6db855b2
Kamran Ahmad <kamranahmadmd463@gmail.com>
2024-05-04 02:47:44 +0530

    create object in main

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        ACCOUNT a1=new ACCOUNT(5000,1234);
+        a1.showbalance();
+        a1.Withdraw();
+
 
     }
 }
